33037902624c992dae632ab31e1e018c0ee425e3
MULTICOMPLEX <thepromisedplanet@gmail.com>
2022-07-02 22:46:42 +0200

    update.

--- function_sim.cpp
+++ function_sim.cpp
@@ -6,157 +6,162 @@
 #include <string>
 #include <numbers>
 
-double EXP_sim(double x = 0.9, size_t n_samples = 10000000000)
+template <typename T, typename L>
+	requires std::floating_point<T>&& std::integral<L>
+T EXP_sim(T x = 0.9, L n_samples = 10000000000)
 {
-  if (x == 0) return 1;
- 
-  mxws_64 rng;
+	if (x == 0) return 1;
 
-  double h = 0;
-  double xi = 1;
-  size_t tot = 0;
+	mxws<uint32_t> rng;
 
-  if (x < -1 || x > 1)
-  {
-    x = std::modf(x, &xi);
-    //The integer part is stored in the object pointed by intpart, 
-    //and the fractional part is returned by the function.
-    xi = double(std::pow(std::numbers::e, xi));
-  }
+	T h = 0;
+	T xi = 1;
+	L tot = 0;
 
-  if (x == 0)return xi;
+	if (x < -1 || x > 1)
+	{
+		x = std::modf(x, &xi);
+		//The integer part is stored in the object pointed by intpart, 
+		//and the fractional part is returned by the function.
+		xi = std::pow(std::numbers::e, xi);
+	}
 
-  if (x > 0)
-  {
-    for (size_t i = 0; i < n_samples; i++)
-    {
-      while (h < 1)
-      {
-        h += rng(0,1.0 / x);
-        tot++;
-      }
-      h = 0; 
-    }
-    return double(tot) / n_samples * xi;
-  }
+	if (x == 0)return xi;
 
-  else {
+	if (x > 0)
+	{
+		for (auto i = 0; i < n_samples; i++)
+		{
+			while (h < 1)
+			{
+				h += rng(1.0 / x);
+				tot++;
+			}
+			h = 0;
+		}
+		return T(tot) / n_samples * xi;
+	}
 
-    x = std::abs(x); 
-    for (size_t i = 0; i < n_samples; i++)
-    {
-      while (h < x)
-      {
-        h += rng(1.0);
-        tot++;
-      }
-      h = 0;
-    }
+	else {
 
-    return n_samples / double(tot) * xi;
-  }
+		x = std::abs(x);
+		for (auto i = 0; i < n_samples; i++)
+		{
+			while (h < x)
+			{
+				h += rng(1.0);
+				tot++;
+			}
+			h = 0;
+		}
+
+		return n_samples / T(tot) * xi;
+	}
 }
 
-double exp_sim(double x, size_t n_samples)
+template <typename T, typename L>
+	requires std::floating_point<T>&& std::integral<L>
+T exp_sim(T x, L n_samples)
 {
-  std::cout << std::endl << "simulated exp(" << x << ") =  ";
-  
-  auto begin = std::chrono::steady_clock::now();
+	std::cout << std::endl << "simulated exp(" << x << ") =  ";
 
-  auto r = EXP_sim(x, n_samples);
+	auto begin = std::chrono::steady_clock::now();
 
-  auto end = std::chrono::steady_clock::now();
+	auto r = EXP_sim(x, n_samples);
 
-  std::cout << std::setprecision(12) << std::fixed << r << std::endl;
+	auto end = std::chrono::steady_clock::now();
 
-  std::cout << std::defaultfloat <<
-    "exact     exp(" << x << ") =  " <<
-    std::setprecision(12) << std::fixed << std::exp(x) << std::endl;
-    
-  std::cout << std::endl;
-  std::cout << "Duration             = " <<
-    std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+	std::cout << std::setprecision(12) << std::fixed << r << std::endl;
 
-  return r;
+	std::cout << std::defaultfloat <<
+		"exact     exp(" << x << ") =  " <<
+		std::setprecision(12) << std::fixed << std::exp(x) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Duration             = " <<
+		std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+	return r;
 }
 
-
-double sqrt_sim(double z = 2, size_t throws = 10000000000)
+template <typename T, typename L>
+	requires std::floating_point<T>&& std::integral<L>
+T sqrt_sim(T z = 2, L throws = 10000000000)
 {
-  mxws_64 rng;
+	mxws<uint32_t> rng;
 
-  size_t tel = 0, i = 0;
-  double r;
-  
-  if (z < 1) {
-    while (i < throws)
-    {
-      r = rng(0,1.0 / z);
-      r *= r;
-      if (r < z)tel++;
-      i++;
-    }
-    return (1.0 / z) * double(tel) / throws;
-  }
-  
-  else {
-    while (i < throws)
-    {
-      r = rng(z);
-      r *= r;
-      if (r < z)tel++;
-      i++;
-    }
-    return z * double(tel) / throws;
-  }
+	uint64_t tel = 0, i = 0;
+	T r;
+
+	if (z < 1) {
+		while (i < throws)
+		{
+			r = rng(1.0 / z);
+			r *= r;
+			if (r < z)tel++;
+			i++;
+		}
+		return (1.0 / z) * T(tel) / throws;
+	}
+
+	else {
+		while (i < throws)
+		{
+			r = rng(z);
+			r *= r;
+			if (r < z)tel++;
+			i++;
+		}
+		return z * T(tel) / throws;
+	}
 }
 
 int main(int argc, char** argv)
 {
-  size_t throws = 100000000;
-  double exp_value = .875, sqrt_value = 2.0;
+	uint64_t throws = 1000000000;
+	double exp_value = .875, sqrt_value = 2.0;
 
-  if (argc == 2) {
-    std::cout << "Usage: function_sim value Nthrows " << std::endl;
-    exp_value = atof(argv[1]);
-   
+	if (argc == 2) {
+		std::cout << "Usage: function_sim value Nthrows " << std::endl;
+		exp_value = atof(argv[1]);
 
-    std::cout << " value    : " << exp_value << std::endl;
-    std::cout << " Nthrows  : " << throws << std::endl << std::endl;
-    exp_sim(exp_value, throws);
-  }
-  else if (argc == 3) {
-    exp_value = atof(argv[1]);
-    std::cout << "exp_value = " << exp_value << std::endl;
-    throws = atoi(argv[2]);
-    exp_sim(exp_value, throws);
-  }
-  
-  else {
-    std::cout << "Usage: function_sim value Nthrows " << std::endl;
-    exp_sim(exp_value, throws);
-  }
 
-  std::cout << std::endl << std::endl;
+		std::cout << " value    : " << exp_value << std::endl;
+		std::cout << " Nthrows  : " << throws << std::endl << std::endl;
+		exp_sim(exp_value, throws);
+	}
+	else if (argc == 3) {
+		exp_value = atof(argv[1]);
+		std::cout << "exp_value = " << exp_value << std::endl;
+		throws = atoi(argv[2]);
+		exp_sim(exp_value, throws);
+	}
 
-  //sqrt sim
-  
-  double r;
-  if (argc == 2) {
-    sqrt_value = atof(argv[1]);
-    r = sqrt_sim(sqrt_value, throws);
-  }
-  else if (argc == 3) {
-    sqrt_value = atof(argv[1]);
-    throws = atoi(argv[2]);
-    sqrt_sim(sqrt_value, throws);
-    r = sqrt_sim(sqrt_value, throws);
-  }
-  else r = sqrt_sim(2.0, throws);
+	else {
+		std::cout << "Usage: function_sim value Nthrows " << std::endl;
+		exp_sim(exp_value, throws);
+	}
 
-  std::cout << std::setprecision(3) << "simulated sqrt(" << sqrt_value << ") = "  <<
-    std::setprecision(12) << r << std::endl << std::setprecision(3) <<
-    "exact sqrt(" << sqrt_value << ")     = " << std::setprecision(12) << sqrt(sqrt_value) << std::endl;
-  
-  return 0;
+	std::cout << std::endl << std::endl;
+
+	//sqrt sim
+
+	double r;
+	if (argc == 2) {
+		sqrt_value = atof(argv[1]);
+		r = sqrt_sim(sqrt_value, throws);
+	}
+	else if (argc == 3) {
+		sqrt_value = atof(argv[1]);
+		throws = atoi(argv[2]);
+		sqrt_sim(sqrt_value, throws);
+		r = sqrt_sim(sqrt_value, throws);
+	}
+	else r = sqrt_sim(2.0, throws);
+
+	std::cout << std::setprecision(3) << "simulated sqrt(" << sqrt_value << ") = " <<
+		std::setprecision(12) << r << std::endl << std::setprecision(3) <<
+		"exact sqrt(" << sqrt_value << ")     = " << std::setprecision(12) << sqrt(sqrt_value) << std::endl;
+
+	return 0;
 }
--- mxws.hpp
+++ mxws.hpp
@@ -1,7 +1,11 @@
-
 #include <random>
-#include <concepts>
+#include <numbers>
+#include <iostream>
 
+template <typename RN>
+	requires
+std::same_as<RN, uint32_t> ||
+std::same_as<RN, uint64_t>
 class mxws
 {
 
@@ -11,16 +15,19 @@ private:
 
 public:
 
-	uint64_t x, w;
+	uint64_t x, w, x1, x2, w1, w2;
 
-	typedef uint32_t result_type;
+	typedef RN result_type;
 
 	void seed()
 	{
 		init();
+		x1 = x2 = 1;
 	}
 
-	mxws(const std::seed_seq& seq)
+	template <typename T>
+		requires	std::same_as<T, std::seed_seq>
+	mxws(const T& seq)
 	{
 		if (seq.size() == 2)
 		{
@@ -28,111 +35,8 @@ public:
 			seq.param(seeds.rbegin());
 			w = (uint64_t(seeds[1]) << 32) | seeds[0];
 			x = 1;
-		}
 
-		else init();
-	}
-
-	mxws()
-	{
-		w = (uint64_t(r()) << 32) | r();
-		x = 1;
-	}
-
-	mxws(const uint64_t& seed)
-	{
-		init();
-	}
-
-	void init()
-	{
-		w = (uint64_t(r()) << 32) | r();
-		x = 1;
-	}
-
-	void init(const uint64_t& seed)
-	{
-		w = seed;
-		x = 1;
-	}
-
-	virtual ~mxws() = default;
-
-	static uint32_t min() { return std::numeric_limits<uint32_t>::min(); }
-	static uint32_t max() { return std::numeric_limits<uint32_t>::max(); }
-
-	inline uint32_t operator()()
-	{
-		x *= w;
-		x = (x >> 32) | (x << 32);
-		w += x;
-		return uint32_t(x);
-	}
-
-	template <typename T>
-		requires std::floating_point<T>
-	inline T operator()(const T& f)
-	{
-		return (*this)() / 4294967296. * f;
-	}
-
-	template <typename T>
-		requires std::floating_point<T>
-	inline T operator()(const T& min, const T& max)
-	{
-		return (*this)(1.0) * (max - min) + min;
-	}
-
-	template <typename T, typename U>
-		requires std::integral<T>&& std::floating_point<U>
-	inline U operator()(const T& min, const U& max)
-	{
-		return (*this)(1.0) * (max - min) + min;
-	}
-
-	template <typename T>
-		requires std::integral<T>
-	inline T operator()(const T& max)
-	{
-		return (*this)() % (max + 1);
-	}
-
-	template <typename T>
-		requires std::integral<T>
-	inline T operator()(const T& min, const T& max)
-	{
-		return min + ((*this)() % (max - min + 1));
-	}
-
-};
-
-
-class mxws_64
-{
-
-private:
-
-	std::random_device r;
-
-public:
-
-	uint64_t x1, x2, w1, w2;
-
-	typedef uint64_t result_type;
-
-	void seed()
-	{
-		init();
-		x1 = x2 = 1;
-	}
-
-	mxws_64(const std::seed_seq& seq)
-	{
-		if (seq.size() == 2)
-		{
-			std::vector<uint32_t> seeds(seq.size());
-			seq.param(seeds.rbegin());
-			w1 = (uint64_t(seeds[1]) << 32) | seeds[0];
+			w1 = w;
 			w2 = w1 + 1;
 			x1 = x2 = 1;
 		}
@@ -140,56 +44,77 @@ public:
 		else init();
 	}
 
-	mxws_64(const uint64_t& seed)
+	mxws()
 	{
-		w1 = (uint64_t(r()) << 32) | r();
-		w2 = seed + w1 + 1;
-		x1 = x2 = 1;
+		init();
 	}
 
-	void init(const uint64_t& seed)
-	{
-		w1 = (uint64_t(r()) << 32) | r();
-		w2 = seed + w1 + 1;
-		x1 = x2 = 1;
-	}
-
-	mxws_64()
+	template <typename T>
+		requires	std::same_as<T, uint64_t>
+	mxws(const T& seed)
 	{
 		init();
 	}
 
 	void init()
 	{
-		w1 = (uint64_t(r()) << 32) | r();
+		w = (uint64_t(r()) << 32) | r();
+		x = 1;
+		w1 = w;
 		w2 = w1 + 1;
 		x1 = x2 = 1;
 	}
 
-	virtual ~mxws_64() = default;
+	template <typename T>
+		requires	std::same_as<T, uint64_t>
+	void init(const T& seed)
+	{
+		w = seed;
+		x = 1;
+	}
 
-	static uint64_t min() { return std::numeric_limits<uint64_t>::min(); }
-	static uint64_t max() { return std::numeric_limits<uint64_t>::max(); }
+	virtual ~mxws() = default;
 
+	static constexpr RN min() { return std::numeric_limits<RN>::min(); }
+	static constexpr RN max() { return std::numeric_limits<RN>::max(); }
 
-	inline uint64_t operator()()
+	inline RN operator()()
+		requires
+	std::same_as<RN, uint32_t>
+	{
+		w += x = std::rotr(x *= w, 32);
+		return RN(x);
+	}
+
+	inline RN operator()()
+		requires
+	std::same_as<RN, uint64_t>
 	{
 		x1 *= w1;
-		x1 = (x1 >> 32) | (x1 << 32);
+		x1 = std::rotr(x1, 32);
 		w1 += x1;
 
 		x2 *= w2;
-		x2 = (x2 >> 32) | (x2 << 32);
+		x2 = std::rotr(x2, 32);
 		w2 += x2;
 
 		return (x1 << 32) | uint32_t(x2);
 	}
 
 	template <typename T>
-		requires std::floating_point<T>
-	inline T operator()(const T& f)
+		requires std::floating_point<T>&&
+	std::same_as<RN, uint64_t>
+		inline T operator()(const T& f)
 	{
-		return ((*this)() >> 11) / 9007199254740992. * f;
+		return ((*this)() >> 11) / T(9007199254740992) * f;
+	}
+
+	template <typename T>
+		requires std::floating_point<T>&&
+	std::same_as<RN, uint32_t>
+		inline T operator()(const T& f)
+	{
+		return (*this)() / T(4294967296) * f;
 	}
 
 	template <typename T>
@@ -220,11 +145,275 @@ public:
 		return min + ((*this)() % (max - min + 1));
 	}
 
+	template <typename T>
+		requires std::floating_point<T>
+	T round_to_half(T in)
+	{
+		in *= 2; in = round(in); return in /= 2;
+	}
+
+	template <typename T>
+		requires std::floating_point<T>
+	inline T to_float(const T& in)
+	{
+		return in / T(4294967296);
+	}
+
+	template <typename T>
+		requires std::integral<T>
+	inline uint32_t to_int(const T& n)
+	{
+		return uint32_t(n >> 32);
+	}
+
+	template <typename T>
+		requires std::floating_point<T>
+	T inline normalRandom(const T& mean, const T& sigma)
+	{
+		// return a normally distributed random value
+		T v1 = (*this)(1.0);
+		T v2 = (*this)(1.0);
+		return std::cos(2 * std::numbers::pi * v2) * std::sqrt(-2 * std::log(v1)) * sigma + mean;
+	}
+
+	template <typename T, typename L>
+		requires std::floating_point<T>&&
+	std::integral<L>
+		T inline error_function(const T& x, const L& iterations)
+	{
+		const auto f = [](const auto& t) {return exp(-pow(t, 2)); };
+
+		T	totalSum = 0;
+
+		double lowBound = 0, upBound = x;
+
+		for (auto i = 0; i < iterations; i++)
+			totalSum += f((*this)(x));
+
+		double estimate = (upBound - lowBound) * totalSum / iterations;
+
+		estimate *= 2 / sqrt(std::numbers::pi);
+
+		std::cout << "error function(" << x << ") = " << estimate << std::endl;
+		std::cout << " std::function(" << x << ") = "
+			<< std::erf(x) << std::endl;
+
+		return estimate;
+	}
+
+	template <typename T>
+		requires std::floating_point<T>
+	T erf_inv(T x) {
+
+		if (x < -1 || x > 1) {
+			return NAN;
+		}
+		else if (x == 1.0) {
+			return INFINITY;
+		}
+		else if (x == -1.0) {
+			return -INFINITY;
+		}
+
+		const long double LN2 = 6.931471805599453094172321214581e-1L;
+
+		const long double A0 = 1.1975323115670912564578e0L;
+		const long double A1 = 4.7072688112383978012285e1L;
+		const long double A2 = 6.9706266534389598238465e2L;
+		const long double A3 = 4.8548868893843886794648e3L;
+		const long double A4 = 1.6235862515167575384252e4L;
+		const long double A5 = 2.3782041382114385731252e4L;
+		const long double A6 = 1.1819493347062294404278e4L;
+		const long double A7 = 8.8709406962545514830200e2L;
+
+		const long double B0 = 1.0000000000000000000e0L;
+		const long double B1 = 4.2313330701600911252e1L;
+		const long double B2 = 6.8718700749205790830e2L;
+		const long double B3 = 5.3941960214247511077e3L;
+		const long double B4 = 2.1213794301586595867e4L;
+		const long double B5 = 3.9307895800092710610e4L;
+		const long double B6 = 2.8729085735721942674e4L;
+		const long double B7 = 5.2264952788528545610e3L;
+
+		const long double C0 = 1.42343711074968357734e0L;
+		const long double C1 = 4.63033784615654529590e0L;
+		const long double C2 = 5.76949722146069140550e0L;
+		const long double C3 = 3.64784832476320460504e0L;
+		const long double C4 = 1.27045825245236838258e0L;
+		const long double C5 = 2.41780725177450611770e-1L;
+		const long double C6 = 2.27238449892691845833e-2L;
+		const long double C7 = 7.74545014278341407640e-4L;
+
+		const long double D0 = 1.4142135623730950488016887e0L;
+		const long double D1 = 2.9036514445419946173133295e0L;
+		const long double D2 = 2.3707661626024532365971225e0L;
+		const long double D3 = 9.7547832001787427186894837e-1L;
+		const long double D4 = 2.0945065210512749128288442e-1L;
+		const long double D5 = 2.1494160384252876777097297e-2L;
+		const long double D6 = 7.7441459065157709165577218e-4L;
+		const long double D7 = 1.4859850019840355905497876e-9L;
+
+		const long double E0 = 6.65790464350110377720e0L;
+		const long double E1 = 5.46378491116411436990e0L;
+		const long double E2 = 1.78482653991729133580e0L;
+		const long double E3 = 2.96560571828504891230e-1L;
+		const long double E4 = 2.65321895265761230930e-2L;
+		const long double E5 = 1.24266094738807843860e-3L;
+		const long double E6 = 2.71155556874348757815e-5L;
+		const long double E7 = 2.01033439929228813265e-7L;
+
+		const long double F0 = 1.414213562373095048801689e0L;
+		const long double F1 = 8.482908416595164588112026e-1L;
+		const long double F2 = 1.936480946950659106176712e-1L;
+		const long double F3 = 2.103693768272068968719679e-2L;
+		const long double F4 = 1.112800997078859844711555e-3L;
+		const long double F5 = 2.611088405080593625138020e-5L;
+		const long double F6 = 2.010321207683943062279931e-7L;
+		const long double F7 = 2.891024605872965461538222e-15L;
+
+		long double abs_x = fabsl(x);
+
+		if (abs_x <= 0.85L) {
+			long double r = 0.180625L - 0.25L * x * x;
+			long double num = (((((((A7 * r + A6) * r + A5) * r + A4) * r + A3) * r + A2) * r + A1) * r + A0);
+			long double den = (((((((B7 * r + B6) * r + B5) * r + B4) * r + B3) * r + B2) * r + B1) * r + B0);
+			return x * num / den;
+		}
+
+		long double r = sqrtl(LN2 - logl(1.0L - abs_x));
+
+		long double num, den;
+		if (r <= 5.0L) {
+			r = r - 1.6L;
+			num = (((((((C7 * r + C6) * r + C5) * r + C4) * r + C3) * r + C2) * r + C1) * r + C0);
+			den = (((((((D7 * r + D6) * r + D5) * r + D4) * r + D3) * r + D2) * r + D1) * r + D0);
+		}
+		else {
+			r = r - 5.0L;
+			num = (((((((E7 * r + E6) * r + E5) * r + E4) * r + E3) * r + E2) * r + E1) * r + E0);
+			den = (((((((F7 * r + F6) * r + F5) * r + F4) * r + F3) * r + F2) * r + F1) * r + F0);
+		}
+
+		return copysignl(num / den, x);
+	}
+
+	template<typename T>
+		requires std::floating_point<T>
+	T inline probit(const T& p)
+	{
+		T root_2 = sqrt(2);
+		return root_2 * erf_inv(2 * p - 1);
+	}
+
+	template <typename R, typename I, typename L>
+		requires
+	std::same_as<R, double>&&
+		std::integral<I>&&
+		std::same_as<L, std::uint64_t>
+		std::tuple<R, I> inline Probability_Wave(const I& board_SIZE,
+			auto& cycle, const I& N_cycles, const L& TRIALS) {
+
+		const I board_size = I(round(log(board_SIZE * 6) * pow(tan(36 / 5.), 2)));
+		const I rn_range = I(floor(board_SIZE / sqrt(log2(board_SIZE))));
+
+		//const I cycle_size = cycle_SIZE;
+		//const I rn_range = I(round(sqrt(cycle_size) + log(cycle_size / 4)));
+
+		L random_walk = 0;
+
+		for (L i = 0; i < TRIALS; i++, random_walk = 0)
+		{
+			for (I j = 0; j < board_size; j++)
+				random_walk += (*this)();
+
+			cycle[to_int(random_walk * rn_range) % board_SIZE]++;
+		}
+
+		return std::make_tuple(rn_range, board_size);
+	}
+
+	template <typename T, typename L>
+		requires std::floating_point<T>&& std::integral<L>
+	T sqrt_mc(T z = 2, L throws = 10000000000)
+	{
+		mxws<uint32_t> rng;
+
+		uint64_t tel = 0, i = 0;
+		T r;
+
+		if (z < 1) {
+			while (i < throws)
+			{
+				r = rng(1.0 / z);
+				r *= r;
+				if (r < z)tel++;
+				i++;
+			}
+			return (1.0 / z) * T(tel) / throws;
+		}
+
+		else {
+			while (i < throws)
+			{
+				r = rng(z);
+				r *= r;
+				if (r < z)tel++;
+				i++;
+			}
+			return z * T(tel) / throws;
+		}
+	}
+
+	template <typename T, typename L>
+		requires std::floating_point<T>&& std::integral<L>
+	T exp_mc(T x = 0.9, L n_samples = 10000000000)
+	{
+		if (x == 0) return 1;
+
+		mxws<uint32_t> rng;
+
+		T h = 0;
+		T xi = 1;
+		L tot = 0;
+
+		if (x < -1 || x > 1)
+		{
+			x = std::modf(x, &xi);
+			//The integer part is stored in the object pointed by intpart, 
+			//and the fractional part is returned by the function.
+			xi = std::pow(std::numbers::e, xi);
+		}
+
+		if (x == 0)return xi;
+
+		if (x > 0)
+		{
+			for (auto i = 0; i < n_samples; i++)
+			{
+				while (h < 1)
+				{
+					h += rng(1.0 / x);
+					tot++;
+				}
+				h = 0;
+			}
+			return T(tot) / n_samples * xi;
+		}
+
+		else {
+
+			x = std::abs(x);
+			for (auto i = 0; i < n_samples; i++)
+			{
+				while (h < x)
+				{
+					h += rng(1.0);
+					tot++;
+				}
+				h = 0;
+			}
+
+			return n_samples / T(tot) * xi;
+		}
+	}
 };
-
-
-
-
-
-
-
