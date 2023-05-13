e6945dfbe4b4140387fd0aacd37c1c67561ae009
MULTICOMPLEX <75379917+MULTICOMPLEX@users.noreply.github.com>
2023-05-13 20:34:24 +0200

    update.

--- log_function.py
+++ log_function.py
@@ -1,16 +1,14 @@
-import random
+#Monte Carlo Log function 
+import numpy as np
 
-def monte_carlo_ln(t, n):
-    """Approximates the natural logarithm of t using Monte Carlo integration
-    with n random points."""
-    total = 0
-    for i in range(n):
-        x = random.uniform(1, t)
-        total += 1 / x
+def mc_log(t, n):
+    x = np.random.uniform(1, t, n)
+    total = np.sum(1/x)
     return (t - 1) * total / n
 
 # Example usage:
-t = 5
-n = 1000000
-approx_ln = monte_carlo_ln(t, n)
-print(f"ln({t}) ≈ {approx_ln:.6f}")
+t = .5
+n = 10000000
+approx_ln = mc_log(t, n)
+print(f"ln({t}) = {approx_ln:.6f}")
+print("exact =", np.log(t))
